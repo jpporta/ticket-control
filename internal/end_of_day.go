cbcb397f5f1f98daed4ad32977f07f6a71afaf67
Joao Porta <jpedro.porta@gmail.com>
2025-08-28 10:41:28 -0300

    fix: end of day query

--- internal/end_of_day.go
+++ internal/end_of_day.go
@@ -13,8 +13,8 @@ import (
 func (a *Application) EndOfDay(ctx context.Context, userId int32, userName string, noDone int) error {
 	taskCreatedToday, err := a.Q.GetNoUsersTask(ctx, repository.GetNoUsersTaskParams{
 		CreatedBy:   userId,
-		CreatedAt:   pgtype.Timestamp{Time: time.Now().Truncate(24 * time.Hour), Valid: true},
-		CreatedAt_2: pgtype.Timestamp{Time: time.Now().Truncate(24 * time.Hour).Add(24 * time.Hour), Valid: true},
+		CreatedAt:   pgtype.Timestamp{Time: time.Now().In(time.UTC).Truncate(24 * time.Hour), Valid: true},
+		CreatedAt_2: pgtype.Timestamp{Time: time.Now().In(time.UTC).Truncate(24 * time.Hour).Add(24 * time.Hour), Valid: true},
 	})
 	if err != nil {
 		return fmt.Errorf("Error getting tasks created today: %w", err)
